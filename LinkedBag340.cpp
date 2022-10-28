10ee203f479573c430d8a06f23ab25104873be6d
Burns <65887526+Dylan-Burns@users.noreply.github.com>
2022-10-28 01:22:43 -0700

    fixed bugs, working solution

--- LinkedBag340.cpp
+++ LinkedBag340.cpp
@@ -1,169 +1,250 @@
-//  LinkedBag.cpp
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//	Updated by Duc Ta
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
+//	LinkedBag340.cpp
+//	Created by: CSC340
 
-#include <cstddef>
-#include "Node.h"
+/*
+===>					<===
+===>  PLEASE ADD CODE	<===
+===>					<===
+*/
+
+
+
 #include "LinkedBag.h"
+#include <iostream>
+#include <cstddef>
+#include <cstdlib>		
+#include <ctime>		
 
-//
-//
-// PLEASE DO NOT CHANGE THIS FILE
-//
-//
 
+
+
+// operations:
+// 
+// step 1: check if the headNode.next != null
+//
+//		headNode -> secondNode -> null
+//					   ???
+// 
+// step 2: we want to assign headNode.next to the secondNode.next to 
+// 
+//		headNode    secondNode -> null
+//			|					   ^
+//			|______________________|
+//			
+// step 3: we want to assign secondNode.next = nullptr
+// 
+//		headNode		secondNode		null
+//			|			    `-> null	 ^
+//			|____________________________|
+// 
+// step 4: we want to remove the secondNode now that it has been unlinked from prevNode and nextNode
+//
+//		headNode -> null
+// this function removes the second node in the linked list //
 template<typename ItemType>
-//create head node and assign its pointer to null | headPtr -> null
-LinkedBag<ItemType>::LinkedBag() : headPtr(nullptr), itemCount(0) {}
+bool LinkedBag<ItemType>::removeSecondNode340() {
 
+	if (headPtr != nullptr) {
+
+		Node<ItemType>* secondNode = headPtr->getNext(); //reference to second Node
+
+		headPtr->setNext(secondNode->getNext());
+		secondNode->setNext(nullptr);
+
+		delete secondNode;
+		secondNode = nullptr;
+		itemCount--;
+
+		return true;
+	}
+	return false;
+}
+// step 1: create a reference to the headNode
+// step 2: traverse linked list until node.next == null (end node found)
+// step 3: create the newNode, add its data, and set newNode.next == nullptr
+// step 3: assign the end node.next to the newNnode
+// step 4: make sure new node added to end points to null
+//
+// this function appends a new node to the end of the linked list
 template<typename ItemType>
-LinkedBag<ItemType>::LinkedBag(const LinkedBag<ItemType>& aBag) {
-	itemCount = aBag.itemCount;
-	//create the first node and assign it to head
-	Node<ItemType>* origChainPtr = aBag.headPtr;
+bool LinkedBag<ItemType>::addEnd340(const ItemType& newEntry) {
+	
+	Node<ItemType>* currPtr = headPtr;
 
-	//if there is not a next node assign headPtr.next to null
-	if (origChainPtr == nullptr) {
-		headPtr = nullptr; 
+	while (currPtr->getNext() != nullptr) {
+		currPtr = currPtr->getNext();
+	}
+	if (currPtr->getNext() == nullptr) {
+		Node<ItemType>* newNode = new Node<ItemType>;
+		newNode->setItem(newEntry);
+		currPtr->setNext(newNode);
+		itemCount++;
+	}
+
+	return true;
+
+}
+
+// step 1: create a count variable initialized to 0 to hold size value
+// step 2: create a while loop to traverse linked list to last node
+// step 3: increment count every iteration
+// step 4: return count
+// 
+//		count = 0
+// 
+//		Node -> Node -> Node -> Node -> null
+//		 `-> 1
+// 
+//		Node -> Node -> Node -> Node -> null
+//				 `-> 2
+// 
+//		Node -> Node -> Node -> Node -> null
+//						 `-> 3
+// 
+//		Node -> Node -> Node -> Node -> null
+//								 `-> 4
+// 
+//		currPtr->getNext() == nullptr -> EXIT LOOP
+// 
+//			count = 4
+//
+// this function iteratively traverses the linked list and returns its size
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Iterative() const {
+
+	int count = 0;
+
+	Node<ItemType>* currPtr = headPtr;
+	while (currPtr != nullptr) {
+		currPtr = currPtr->getNext();
+		count++;
+	}
+	return count;
+}
+
+// This function uses an auxiliary function to compute the size of
+// the linked list (# of nodes) and return value back its caller
+//
+// this function uses the auxiliary function below to recursively traverse 
+// the linked list and find its size
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Recursive() const {
+
+	Node<ItemType>* currPtr = headPtr;
+	return getCurrentSize340RecursiveHelper(currPtr);
+}
+
+// recursive auxiliary function
+// step 1: base case | check if headNode == null
+// step 2: return recursive function call passing headNode.next as a param
+// function will exit when base case is met ( node == null)
+//
+// this is an auxiliary function used by the function above
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>* currPtr) const {
+
+	// base Case
+	if (currPtr == nullptr) {
+		return 0;
+	}
+	// count this node plus the rest of the list
+	else {
+		return 1 + getCurrentSize340RecursiveHelper(currPtr->getNext());
+	}
+}
+
+// IMMEDIATE RECURSION
+// recursive function to compute linked list size without an auxiliary function
+// step 1: base case: check if headNode == null
+// step 2: return recursive function call passing headNode.next as param
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {
+
+	Node<ItemType>* currPtr = headPtr;
+	if (currPtr == nullptr) {
+		return 0;
 	}
 	else {
-		//link head pointe to new node in linked list
-		headPtr = new Node<ItemType>();
-		//set head pointer data to same value as origChainPtr data
-		headPtr->setItem(origChainPtr->getItem());
+		auto duplicate = LinkedBag(*this);
+		duplicate.remove(duplicate.headPtr->getItem());
+		return 1 + duplicate.getCurrentSize340RecursiveNoHelper();
+	}
+}
 
-		Node<ItemType>* newChainPtr = headPtr; 
-		origChainPtr = origChainPtr->getNext();
+// this function uses an auziliary function to compute the size of the linked list
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType& anEntry) const {
+	Node<ItemType>* currPtr = headPtr;
+	return getFrequencyOf340RecursiveHelper(currPtr, anEntry);
+}
 
-		while (origChainPtr != nullptr)
-		{
-			ItemType nextItem = origChainPtr->getItem();
-			Node<ItemType>* newNodePtr = new Node<ItemType>(nextItem);
-			newChainPtr->setNext(newNodePtr);
-			newChainPtr = newChainPtr->getNext();
-			origChainPtr = origChainPtr->getNext();
-		} 
+// recursive auxiliary function
+// step 1: base case | check if headNode == null
+// step 2: return recursive function call passing headNode.next as a param
+// function will exit when base case is met ( node == null)
+// 
+// this is an auxiliary function used by the function above
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>* currPtr, const ItemType& anEntry) const {
 
-		newChainPtr->setNext(nullptr);
+
+	if (currPtr == nullptr) {
+		return 0;
+	}
+	if (currPtr->getItem() == anEntry) {
+		return 1 + getFrequencyOf340RecursiveHelper(currPtr->getNext(), anEntry);
+	}
+	else {
+		return getFrequencyOf340RecursiveHelper(currPtr->getNext(), anEntry);
+	}
+
+}
+
+
+// IMMEDIATE RECURSION
+// step 1: initialize count = 0 to track frequency
+
+// step 2: traverse linked list and increment count when match is found
+// step 3: return count
+template<typename ItemType> 
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType& anEntry) const {
+
+	static Node<ItemType>* currPtr = headPtr;
+	int cases = 0;
+	if (currPtr == nullptr) {
+		currPtr = headPtr;
+		return 0;
+	}
+	if (currPtr->getItem() == anEntry) {
+		cases++;
+		currPtr = currPtr->getNext();
+		return cases + getFrequencyOf340RecursiveNoHelper(anEntry);
+	}
+	else {
+		currPtr = currPtr->getNext();
+		return cases + getFrequencyOf340RecursiveNoHelper(anEntry);
 	}
 }
 
 template<typename ItemType>
-LinkedBag<ItemType>::~LinkedBag() {
-	clear();
-} 
+ItemType LinkedBag<ItemType>::removeRandom340() {
+	int random = 0;
+	int bagSize = getCurrentSize340Iterative();
+	int min = 1;
+	ItemType item;
 
-template<typename ItemType>
-bool LinkedBag<ItemType>::isEmpty() const {
-	return itemCount == 0;
-}
-
-template<typename ItemType>
-int LinkedBag<ItemType>::getCurrentSize() const {
-	return itemCount;
-}
-
-template<typename ItemType>
-bool LinkedBag<ItemType>::add(const ItemType& newEntry) {
-	Node<ItemType>* nextNodePtr = new Node<ItemType>();
-	nextNodePtr->setItem(newEntry);
-	nextNodePtr->setNext(headPtr);  
-	headPtr = nextNodePtr;
-	itemCount++;
-	return true;
-}
-
-template<typename ItemType>
-std::vector<ItemType> LinkedBag<ItemType>::toVector() const {
-	std::vector<ItemType> bagContents;
-	Node<ItemType>* curPtr = headPtr;
-	int counter = 0;
-
-	while ((curPtr != nullptr) && (counter < itemCount)) {
-		bagContents.push_back(curPtr->getItem());
-		curPtr = curPtr->getNext();
-		counter++;
+	for (int i = 1; i <= bagSize; i++) {
+		random = rand() % bagSize + min;
 	}
 
-	return bagContents;
-}
+	Node<ItemType>* currPtr = headPtr;
+	int nElement = random;
 
-//remove the node from the linked list
-template<typename ItemType>
-bool LinkedBag<ItemType>::remove(const ItemType& anEntry) {
-	Node<ItemType>* entryNodePtr = getPointerTo(anEntry);
-	//if the linked list is not empty and the node != null
-	bool canRemoveItem = !isEmpty() && (entryNodePtr != nullptr);
-
-	if (canRemoveItem) {
-		entryNodePtr->setItem(headPtr->getItem());
-		Node<ItemType>* nodeToDeletePtr = headPtr;
-		headPtr = headPtr->getNext();
-
-		nodeToDeletePtr->setNext(nullptr);
-		delete nodeToDeletePtr;
-		nodeToDeletePtr = nullptr;
-
-		itemCount--;
+	for (int i = 0; i < (nElement - 2); i++) {
+		currPtr = currPtr->getNext();
 	}
-
-	return canRemoveItem;
+	Node<ItemType>* temp = currPtr->getNext();
+	item = temp->getItem();
+	remove(item);
+	return item;
 }
-
-template<typename ItemType>
-void LinkedBag<ItemType>::clear() {
-	Node<ItemType>* nodeToDeletePtr = headPtr;
-
-	while (headPtr != nullptr) {
-		headPtr = headPtr->getNext();
-		nodeToDeletePtr->setNext(nullptr);
-		delete nodeToDeletePtr;
-		nodeToDeletePtr = headPtr;
-	}
-
-	itemCount = 0;
-} 
-
-template<typename ItemType>
-int LinkedBag<ItemType>::getFrequencyOf(const ItemType& anEntry) const {
-	int frequency = 0;
-	int counter = 0;
-	Node<ItemType>* curPtr = headPtr;
-
-	while ((curPtr != nullptr) && (counter < itemCount)) {
-		if (anEntry == curPtr->getItem()) {
-			frequency++;
-		} 
-		counter++;
-		curPtr = curPtr->getNext();
-	}
-
-	return frequency;
-}
-
-template<typename ItemType>
-bool LinkedBag<ItemType>::contains(const ItemType& anEntry) const {
-	return (getPointerTo(anEntry) != nullptr);
-}
-
-template<typename ItemType>
-Node<ItemType>* LinkedBag<ItemType>::getPointerTo(const ItemType& anEntry) const {
-	bool found = false;
-	//sets current node to headNode
-	Node<ItemType>* curPtr = headPtr;
-
-	//search linked list starting from head until you reach null or find the node
-	while (!found && (curPtr != nullptr)) {
-		//if the node to be found = current node then we found the node
-		if (anEntry == curPtr->getItem()) {
-			found = true;
-		}
-		//else set current node to the next node
-		else {
-			curPtr = curPtr->getNext();
-		}
-	}
-
-	return curPtr;
-} 

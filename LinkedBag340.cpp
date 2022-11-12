492e00bffb7086fba5339cfb5ea704697323b8de
Burns <65887526+Dylan-Burns@users.noreply.github.com>
2022-11-11 22:49:12 -0800

    Update LinkedBag340.cpp

--- LinkedBag340.cpp
+++ LinkedBag340.cpp
@@ -1,3 +1,5 @@
+
+
 //	LinkedBag340.cpp
 //	Created by: CSC340
 
@@ -6,35 +8,30 @@
 ===>  PLEASE ADD CODE	<===
 ===>					<===
 */
-
-
 #include "Include.h"
 #include <iostream>
 #include <cstddef>
-#include <cstdlib>
-#include <ctime>
-
-
-
+#include <cstdlib>		
+#include <ctime>		
 
 // operations:
 // 
 // step 1: check if the headNode.next != null
 //
 //		headNode -> secondNode -> null
-//			       ???
+//					   ???
 // 
 // step 2: we want to assign headNode.next to the secondNode.next to 
 // 
 //		headNode    secondNode -> null
-//		   |		           ^
-//		   |_______________________|
+//			|					   ^
+//			|______________________|
 //			
 // step 3: we want to assign secondNode.next = nullptr
 // 
-//		headNode	secondNode	null
-//	           |		  `-> null	 ^
-//		   |_____________________________|
+//		headNode		secondNode		null
+//			|			    `-> null	 ^
+//			|____________________________|
 // 
 // step 4: we want to remove the secondNode now that it has been unlinked from prevNode and nextNode
 //
@@ -45,17 +42,16 @@ bool LinkedBag<ItemType>::removeSecondNode340() {
 
 	if (headPtr != nullptr) {
 
-		Node<ItemType>* secondNode = headPtr->getNext(); //reference to second Node
+		auto secondNode = headPtr->getNext(); //reference to second Node
 
 		headPtr->setNext(secondNode->getNext());
 		secondNode->setNext(nullptr);
 
-		delete secondNode;
-		secondNode = nullptr;
 		itemCount--;
 
 		return true;
 	}
+
 	return false;
 }
 // step 1: create a reference to the headNode
@@ -67,21 +63,21 @@ bool LinkedBag<ItemType>::removeSecondNode340() {
 // this function appends a new node to the end of the linked list
 template<typename ItemType>
 bool LinkedBag<ItemType>::addEnd340(const ItemType& newEntry) {
-	
-	Node<ItemType>* currPtr = headPtr;
+
+	auto currPtr = headPtr;
 
 	while (currPtr->getNext() != nullptr) {
 		currPtr = currPtr->getNext();
 	}
+
 	if (currPtr->getNext() == nullptr) {
-		Node<ItemType>* newNode = new Node<ItemType>;
+		auto newNode = std::make_unique< Node<ItemType> >();
 		newNode->setItem(newEntry);
 		currPtr->setNext(newNode);
 		itemCount++;
 	}
 
 	return true;
-
 }
 
 // step 1: create a count variable initialized to 0 to hold size value
@@ -95,13 +91,13 @@ bool LinkedBag<ItemType>::addEnd340(const ItemType& newEntry) {
 //		 `-> 1
 // 
 //		Node -> Node -> Node -> Node -> null
-//			 `-> 2
+//				 `-> 2
 // 
 //		Node -> Node -> Node -> Node -> null
-//				 `-> 3
+//						 `-> 3
 // 
 //		Node -> Node -> Node -> Node -> null
-//					 `-> 4
+//								 `-> 4
 // 
 //		currPtr->getNext() == nullptr -> EXIT LOOP
 // 
@@ -113,11 +109,13 @@ int LinkedBag<ItemType>::getCurrentSize340Iterative() const {
 
 	int count = 0;
 
-	Node<ItemType>* currPtr = headPtr;
+	auto currPtr = headPtr;
+
 	while (currPtr != nullptr) {
 		currPtr = currPtr->getNext();
 		count++;
 	}
+
 	return count;
 }
 
@@ -129,8 +127,9 @@ int LinkedBag<ItemType>::getCurrentSize340Iterative() const {
 template<typename ItemType>
 int LinkedBag<ItemType>::getCurrentSize340Recursive() const {
 
-	Node<ItemType>* currPtr = headPtr;
-	return getCurrentSize340RecursiveHelper(currPtr);
+	auto currPtr = headPtr;
+
+	return getCurrentSize340RecursiveHelper(std::make_unique<Node<ItemType>>(currPtr));
 }
 
 // recursive auxiliary function
@@ -140,7 +139,7 @@ int LinkedBag<ItemType>::getCurrentSize340Recursive() const {
 //
 // this is an auxiliary function used by the function above
 template<typename ItemType>
-int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>* currPtr) const {
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(std::unique_ptr< Node<ItemType> > currPtr) const {
 
 	// base Case
 	if (currPtr == nullptr) {
@@ -148,7 +147,7 @@ int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>* currPt
 	}
 	// count this node plus the rest of the list
 	else {
-		return 1 + getCurrentSize340RecursiveHelper(currPtr->getNext());
+		return 1 + getCurrentSize340RecursiveHelper(std::make_unique<Node<ItemType>>(currPtr->getNext()));
 	}
 }
 
@@ -159,13 +158,15 @@ int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>* currPt
 template<typename ItemType>
 int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {
 
-	Node<ItemType>* currPtr = headPtr;
+	auto currPtr = headPtr;
+
 	if (currPtr == nullptr) {
 		return 0;
 	}
 	else {
 		auto duplicate = LinkedBag(*this);
 		duplicate.remove(duplicate.headPtr->getItem());
+
 		return 1 + duplicate.getCurrentSize340RecursiveNoHelper();
 	}
 }
@@ -173,8 +174,10 @@ int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {
 // this function uses an auziliary function to compute the size of the linked list
 template<typename ItemType>
 int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType& anEntry) const {
-	Node<ItemType>* currPtr = headPtr;
-	return getFrequencyOf340RecursiveHelper(currPtr, anEntry);
+
+	auto currPtr = headPtr;
+
+	return getFrequencyOf340RecursiveHelper(std::make_unique<Node<ItemType>>(currPtr), anEntry);
 }
 
 // recursive auxiliary function
@@ -184,17 +187,17 @@ int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType& anEntry) con
 // 
 // this is an auxiliary function used by the function above
 template<typename ItemType>
-int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>* currPtr, const ItemType& anEntry) const {
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(std::unique_ptr< Node<ItemType> > currPtr, const ItemType& anEntry) const {
 
 
 	if (currPtr == nullptr) {
 		return 0;
 	}
 	if (currPtr->getItem() == anEntry) {
-		return 1 + getFrequencyOf340RecursiveHelper(currPtr->getNext(), anEntry);
+		return 1 + getFrequencyOf340RecursiveHelper(std::make_unique< Node<ItemType> >((currPtr->getNext())), anEntry);
 	}
 	else {
-		return getFrequencyOf340RecursiveHelper(currPtr->getNext(), anEntry);
+		return getFrequencyOf340RecursiveHelper(std::make_unique< Node<ItemType> >((currPtr->getNext())), anEntry);
 	}
 
 }
@@ -205,45 +208,53 @@ int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>* currPt
 
 // step 2: traverse linked list and increment count when match is found
 // step 3: return count
-template<typename ItemType> 
+template<typename ItemType>
 int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType& anEntry) const {
 
-	static Node<ItemType>* currPtr = headPtr;
+	static auto currPtr = headPtr;
 	int cases = 0;
+
 	if (currPtr == nullptr) {
 		currPtr = headPtr;
+
 		return 0;
 	}
 	if (currPtr->getItem() == anEntry) {
 		cases++;
 		currPtr = currPtr->getNext();
+
 		return cases + getFrequencyOf340RecursiveNoHelper(anEntry);
 	}
 	else {
 		currPtr = currPtr->getNext();
+
 		return cases + getFrequencyOf340RecursiveNoHelper(anEntry);
 	}
 }
 
 template<typename ItemType>
 ItemType LinkedBag<ItemType>::removeRandom340() {
+
 	int random = 0;
 	int bagSize = getCurrentSize340Iterative();
 	int min = 1;
+
 	ItemType item;
 
 	for (int i = 1; i <= bagSize; i++) {
 		random = rand() % bagSize + min;
 	}
 
-	Node<ItemType>* currPtr = headPtr;
+	auto currPtr = headPtr;
 	int nElement = random;
 
 	for (int i = 0; i < (nElement - 2); i++) {
 		currPtr = currPtr->getNext();
 	}
-	Node<ItemType>* temp = currPtr->getNext();
+
+	auto temp = currPtr->getNext();
 	item = temp->getItem();
 	remove(item);
+
 	return item;
 }

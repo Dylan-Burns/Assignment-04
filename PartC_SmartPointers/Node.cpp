2250e800a37b201ea9bbddac85caf1a889115374
Burns <65887526+Dylan-Burns@users.noreply.github.com>
2022-11-11 22:56:47 -0800

    Update Node.cpp

--- PartC_SmartPointers/Node.cpp
+++ PartC_SmartPointers/Node.cpp
@@ -4,6 +4,7 @@
 //  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
 #include "Node.h"
+#include <memory>
 
 //
 //
@@ -11,31 +12,38 @@
 //
 //
 
+//creates a new node with no data and points to null (no nextNode)
 template<typename ItemType>
 Node<ItemType>::Node() : item(), next(nullptr) {}
 
+//creates a new Node initialized with data and points to null
 template<typename ItemType>
 Node<ItemType>::Node(const ItemType& anItem) : item(anItem), next(nullptr) {}
 
+//creates a new Node initialized with data and points to a nextNode
 template<typename ItemType>
 Node<ItemType>::Node(const ItemType& anItem, Node<ItemType>* nextNodePtr) :
 	item(anItem), next(nextNodePtr) {} 
 
+//sets caller data of the Node
 template<typename ItemType>
 void Node<ItemType>::setItem(const ItemType& anItem) {
 	item = anItem;
 }
 
+//sets caller Node.next 
 template<typename ItemType>
 void Node<ItemType>::setNext(Node<ItemType>* nextNodePtr) {
 	next = nextNodePtr;
 }
 
+//retrieves caller node data
 template<typename ItemType>
 ItemType Node<ItemType>::getItem() const {
 	return item;
 }
 
+//retrieves caller Node.next
 template<typename ItemType>
 Node<ItemType>* Node<ItemType>::getNext() const {
 	return next;
